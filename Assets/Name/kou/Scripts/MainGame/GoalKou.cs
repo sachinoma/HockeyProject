895387e27ac0b9491f10f9850366134bcfd89dd2
22ci0303 <22ci0303@jec.ac.jp>
2023-07-19 11:47:14 +0900

    ゴールエフェクト追加

--- Assets/Name/kou/Scripts/MainGame/GoalKou.cs
+++ Assets/Name/kou/Scripts/MainGame/GoalKou.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class GoalKou : MonoBehaviour
@@ -13,6 +14,9 @@ public class GoalKou : MonoBehaviour
     [SerializeField]
     private bool isLeft;
 
+    [SerializeField]
+    GameObject goalFX;
+
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManagerKou>();
@@ -21,12 +25,13 @@ public class GoalKou : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Hockey")
+        if (other.gameObject.tag == "Hockey")
         {
             audioSource.PlayOneShot(goal, 1.0f);
             gameManager.HitEvent();
             gameManager.ScorePlus(isLeft, 1);//スコアを1加算
             gameManager.TriggerSpawnHockey();//ホッケー再生成
+            Instantiate(goalFX, transform.position, Quaternion.identity);
             Destroy(other.gameObject.transform.parent.gameObject);//今衝突しているホッケーをdestroy
         }
     }
